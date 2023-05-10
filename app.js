f452675a401e7343560ce002fe8ed9e87c41140f
DiptenduMondal28 <diptendumondal.2801@gmail.com>
2023-05-10 12:39:56 +0530

    add post and use method also parse body

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express=require('express');
+const bodyParser=require('body-parser');
 
 const app=express();
+app.use(bodyParser.urlencoded({extended:false}));
 
-app.use((req,res,next)=>{
-    console.log("first middlewire");
-    next();
-})
+app.use('/add-product',(req,res,next)=>{
+    //console.log(req);
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name ="size"><button type="submit">Add Product</button></form>');
+});
 
-app.use((re,res,next)=>{
-    console.log('second middlewire');
-    res.send('<h1>Use the express first time!</h1>')
-})
+app.post('/product',(req,res,next)=>{
+    console.log("req body")
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>You are using Express!</h1>')
+});
 
 app.listen(3000);
